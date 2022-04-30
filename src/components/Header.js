0ed25fc800591bb3368d4a2dbc5020562be4755a
RogueUchiha <shortbenji2@gmail.com>
2022-04-30 16:54:13 -0400

    changed cart to cart icon and added a badge to it

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,12 +2,23 @@ import { Navbar, Nav, Container } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 import { getAuth } from "firebase/auth";
 import { useAuthStatus } from "../hooks/useAuthStatus";
+import { Badge } from "@mui/material";
+import { ShoppingCart } from "@mui/icons-material";
 import logo from "../../src/wvutechlogo-png.png";
+import { createTheme, ThemeProvider, styled } from "@mui/material/styles";
 
 const Header = () => {
   const { loggedIn, setLoggedIn } = useAuthStatus();
   const auth = getAuth();
   const navigate = useNavigate();
+
+  const theme = createTheme({
+    palette: {
+      secondary: {
+        main: "#eaaa00",
+      },
+    },
+  });
 
   const onLogout = () => {
     console.log("hello");
@@ -17,40 +28,48 @@ const Header = () => {
   };
 
   return (
-    <header>
-      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
-        <Container>
-          <Navbar.Brand href="/">
-            <img
-              src={logo}
-              width="38"
-              height="38"
-              className="d-inline-block align-top"
-              alt="React Bootstrap logo"
-            />{" "}
-            <h3 className="d-inline-block">WVU Tech Bookstore</h3>
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ms-auto">
-              <Nav.Link href="/cart">Cart</Nav.Link>
-              {loggedIn ? (
-                <>
-                  <Nav.Link href="/manageproducts">Product Management</Nav.Link>
-                  <Nav.Link onClick={onLogout}>Sign Out</Nav.Link>
-                </>
-              ) : (
-                // <button type="button" onClick={onLogout}>
-                //   Sign Out
-                // </button>
-                <Nav.Link href="/signin">Sign In</Nav.Link>
-              )}
-              {/* <Nav.Link href="/signin">Sign In</Nav.Link> */}
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    </header>
+    <ThemeProvider theme={theme}>
+      <header>
+        <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+          <Container>
+            <Navbar.Brand href="/">
+              <img
+                src={logo}
+                width="38"
+                height="38"
+                className="d-inline-block align-top"
+                alt="React Bootstrap logo"
+              />{" "}
+              <h3 className="d-inline-block">WVU Tech Bookstore</h3>
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="ms-auto">
+                <Nav.Link href="/cart">
+                  <Badge color="secondary" badgeContent={0}>
+                    <ShoppingCart />
+                  </Badge>
+                </Nav.Link>
+                {loggedIn ? (
+                  <>
+                    <Nav.Link href="/manageproducts">
+                      Product Management
+                    </Nav.Link>
+                    <Nav.Link onClick={onLogout}>Sign Out</Nav.Link>
+                  </>
+                ) : (
+                  // <button type="button" onClick={onLogout}>
+                  //   Sign Out
+                  // </button>
+                  <Nav.Link href="/signin">Sign In</Nav.Link>
+                )}
+                {/* <Nav.Link href="/signin">Sign In</Nav.Link> */}
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </header>
+    </ThemeProvider>
   );
 };
 
